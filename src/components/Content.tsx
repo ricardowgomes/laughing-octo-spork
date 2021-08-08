da8cb2ec5ff680061aff47956b5df3b113e5d318
Ricardo Wagner Gomes <ricardowgomes@outlook.com>
2021-08-08 10:59:10 -0600

    separed components from App.tsx

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,23 @@
-export function Content() {
-  // Complete aqui
-}
+import { MovieCard } from './MovieCard';
+import '.././styles/content.scss';
+
+export function Content(props) {
+  console.log("this are props", props);
+  const moviesCard = props.movies.map(movie => (
+    <MovieCard key={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+  ))
+
+  return (
+    <>
+      <header>
+        <span className="category">Categoria:<span> {props.selectedGenre.title}</span></span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {moviesCard}
+        </div>
+      </main>
+    </>
+  )
+};
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,25 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from './Button';
+import '.././styles/sidebar.scss';
+
+export function SideBar(props) {
+  const mapGenres = props.genres.map(genre => (
+    <Button
+      key={String(genre.id)}
+      title={genre.title}
+      iconName={genre.name}
+      onClick={() => props.onClick(genre.id)}
+      selected={props.selectedGenreId === genre.id}
+    />
+  ))
+
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {mapGenres}
+      </div>
+
+    </nav>
+  )
 }
